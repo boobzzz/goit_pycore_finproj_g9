5ff718b8aae7b3e5c2b84ed8a56b93e87fff8c6e
Serhiy R. <s.ryabyy@gmail.com>
2024-08-16 06:16:38 -0700

    added the search system for name, phone, birthday, address and address subfields

--- record.py
+++ record.py
@@ -3,6 +3,7 @@ from name import Name
 from phone import Phone, PhoneData
 from commands import Commands
 from birthday import Birthday
+import re
 
 
 class Record:
@@ -76,3 +77,49 @@ class Record:
 
     def update_address(self, params: AddressParams):
         self.__address = Address(params)
+
+    def find_match(self, field: str, query: str, perfect_match: bool) -> bool:
+        query = query.casefold()
+        query = re.escape(query)
+        if perfect_match:
+            if query[0] != "^": query = "^" + query
+            if query[-1] != "$": query = query + "$"
+        else:
+            query = query.replace("%", ".*")
+            query = query.replace("_", ".{1}")
+        match field:
+            case Commands.NAME:
+                string = self.name.value.casefold()
+                result = re.search(query, string)
+                return bool(result)
+            case Commands.PHONE:
+                for phone in self.phones:
+                    string = phone.value
+                    result = re.search(query, string)
+                    if result: return True
+            case Commands.BIRTHDAY:
+                if self.birthday is not None:
+                    string = str(self.birthday)
+                    result = re.search(query, string)
+                    return bool(result)
+            case Commands.ADDRESS:
+                if self.address is not None:
+                    string = self.address.value.casefold()
+                    result = re.search(query, string)
+                    return bool(result)
+            case Commands.CITY:
+                if self.address is not None:
+                    string = self.address.city.casefold()
+                    result = re.search(query, string)
+                    return bool(result)
+            case Commands.STREET:
+                if self.address is not None:
+                    string = self.address.street.casefold()
+                    result = re.search(query, string)
+                    return bool(result)
+            case Commands.BUILDING:
+                if self.address is not None:
+                    string = self.address.building.casefold()
+                    result = re.search(query, string)
+                    return bool(result)
+        return False
